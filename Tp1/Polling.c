a96d76dbc139dcb52703b042faef84e223f4e96b
Luciano <lucianoserruya@hotmail.com>
2016-03-21 14:04:35 -0300

    Polling con XOR
    Escribe los puertos solamente cuando se realiza un cambio (trabaja igual
    que el de automatismo)

--- Tp1/Polling.c
+++ Tp1/Polling.c
@@ -11,6 +11,16 @@
 #include <stdlib.h>
 #include "p33FJ256GP710.h"
 
+/*Variables globales*/
+
+int encendido1 = 0;
+int encendido2 = 0;
+int anterior1 = 1;
+int actual1 = 1;
+int anterior2 = 1;
+int actual2 = 1;
+int hayCambio1 = 1;
+int hayCambio2 = 1;
 
 
 void config (void)
@@ -22,37 +32,41 @@ void config (void)
 
 void inspecciona1 (void)
 {
-    
-    while (PORTDbits.RD6 == 0)
+	actual1 = PORTDbits.RD6;
+	hayCambio1 = anterior1 ^ actual1;
+		
+		
+	if (hayCambio1) 
 	{
-		PORTAbits.RA0 = 1;		//El led se mantendra encendido siempre y cuando se este pulsando el boton
+		encendido1 = encendido1 ^ hayCambio1;
+		PORTAbits.RA0 = encendido1;
 	}
-    PORTAbits.RA0 = 0;	//Cuando no se lo este pulsando mas, se apaga
-
+	
+	anterior1 = actual1;
 }
 void inspecciona2 (void)
 {
-
-    
-    while (PORTDbits.RD7 == 0)
+	actual2 = PORTDbits.RD7;
+	hayCambio2 = anterior2 ^ actual2;
+		
+		
+	if (hayCambio2) 
 	{
-		PORTAbits.RA1 = 1;	
+		encendido2 = encendido2 ^ hayCambio2;
+		PORTAbits.RA1 = encendido2;
 	}
-    PORTAbits.RA1 = 0;
-    
+	
+	anterior2 = actual2;
 }
-
-
 
 int main(int argc, char** argv) 
 {
-    
     config();
     
     while(1)
     {   
-        inspecciona1();
-        inspecciona2();
+		inspecciona1();
+		inspecciona2();
     }
     
     return (EXIT_SUCCESS);
