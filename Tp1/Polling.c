14ebd0f00b0173ab398d3505f55e0b90e2e646b8
Luciano <lucianoserruya@hotmail.com>
2016-03-22 09:40:18 -0300

    Comentado
    Se agregaron comentarios. No se modificó el código

--- Tp1/Polling.c
+++ Tp1/Polling.c
@@ -27,18 +27,18 @@ void config (void)
 {
     TRISD = 192; //1100 0000
     TRISA = 0;
-    PORTA = 0;  //Inicializamos el puerto en cero
+    PORTA = 0;  //Se configura el PORTA para salidas
 }
 
 void inspecciona1 (void)
 {
 	actual1 = PORTDbits.RD6;
-	hayCambio1 = anterior1 ^ actual1;
+	hayCambio1 = anterior1 ^ actual1;		//Se consulta si el estado del pin cambió de la 
+											//última vez que se analizó
 		
-		
-	if (hayCambio1) 
+	if (hayCambio1) 						//Si hubo un cambio
 	{
-		encendido1 = encendido1 ^ hayCambio1;
+		encendido1 = encendido1 ^ hayCambio1;	//Se prende/apaga el led
 		PORTAbits.RA0 = encendido1;
 	}
 	
