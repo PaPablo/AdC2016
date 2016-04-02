ce49a6586aadecace398c2c8ffe96834e38755b4
Pablo <pabloatm980@gmail.com>
2016-04-02 18:13:49 -0300

    Tp2 Prog1 Funciona Correctamente (sujeto a cambios)
    Sujeto a cambios, por si no leiste en el titulo.
    
    So re loco
    medio opa

--- TP2/Buffer.c
+++ TP2/Buffer.c
@@ -12,19 +12,93 @@
 
 /*
  * 
+ * 
+ *
  */
 
+
+int iBuffer = 0; 
 typedef struct {
 
 		unsigned int esNuevo:1; //1 bit para la bandera que indica un caracter nuevo en buffer
 		unsigned char carac: 8;	// el caracter en esa posicion del buffer
-		unsigned : 9;			//resto del campo inaccesible;
+		unsigned : 7;			//resto del campo inaccesible;
 
 	} tBuffer;
     
-    
-int main(int argc, char** argv) {
+const int MAXBUFFER = 20;
 
+tBuffer buffer[20];         //tama�o del buffer
+unsigned char tabla[1000];  //Tabla en memoria para tirar valores ingresados por PORTA
+
+void config (void){
+    
+    int i;
+    AD1PCFGL = 0xFFFF;
+    AD1PCFGH = 0xFFFF;
+    AD2PCFGL = 0xFFFF;
+    TRISA = 0X00FF;
+    
+    
+    INTCON2bits.INT0EP = 1; //Configura INT0 para que interrumpa 
+                            //en flanco descendente
+    
+    IFS0bits.INT0IF = 0; 	//INTERRUPT FLAG
+    IEC0bits.INT0IE = 1; 	//INTERRUPT ENABLE
+
+    for(i = 0;i < MAXBUFFER ; i++)
+    {
+        buffer[i].esNuevo = 0;
+    }
+
+}
+
+//extern int iBuffer;
+
+void __attribute__((interrupt, auto_psv)) _INT0Interrupt( void ) {
+
+    IFS0bits.INT0IF = 0;    // Bajamos la bandera de interrupci�n
+    
+    buffer[iBuffer].carac = PORTA;
+    buffer[iBuffer].esNuevo = 1;
+    
+    if(iBuffer == (MAXBUFFER - 1)) {
+        iBuffer = 0;        // Pregunta si ya llego a la ultima posicion
+    } else {                // del buffer, si lleg� reinicia el indice. 
+        iBuffer++;          // En caso contrario incrementa.
+    }
+    
+    
+    
+    
+}
+
+
+int main(int argc, char** argv) {
+    config();
+    int j = 0;
+    int i;
+	while(1){
+        
+        
+        
+        for(i = 0; i < MAXBUFFER; i++) {
+            if(buffer[i].esNuevo){
+                
+                tabla[j] = buffer[i].carac;
+                buffer[i].esNuevo = 0;
+                
+                j++;
+                
+            }
+        }  
+ 
+
+			// recorre el buffer buscando caracteres nuevos
+			// en caso de encontrar, los desposita en la tabla
+			// en memoria
+
+	}
     return (EXIT_SUCCESS);
 }
 
