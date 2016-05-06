65948e65c1134d7b4ee51153c042d98a094cebac
Luciano <lucianoserruya@hotmail.com>
2016-05-06 11:43:01 -0300

    Muchos comentarios

--- tp3/configs.c
+++ tp3/configs.c
@@ -1,47 +1,53 @@
 void configADC(void) {
+//COMENTARIOS CON L ADELANTE SON DE "LOS HIZO LUCIANO"	
+	
+	
+	
 	// ---------- CONFIGURACION ADC ----------
 	//seleccionar fuente de comienzo de sampling (comienzo automatico cuando el contador termina los tads)
 	
-	AD1CON1bits.SSRC = 7;
+	AD1CON1bits.SSRC = 7;										//L: ESTO ESTA RE BIEN
 
 	//sampleo sumultaneo del canal CH0 y CH1
-	AD1CON1bits.SIMSAM = 0;
+	AD1CON1bits.SIMSAM = 0;										//L: NO HACE FALTA PERO ESTA BIEN IGUALMENTE
 
 	//Sampleo comienza automaticamente luego de la ultima conversion
-	AD1CON1bits.ASAM = 1;
+	AD1CON1bits.ASAM = 1;										//L: RE BIEN
 
 	//Buffer DMA es escrito en orden de conversión
-	AD1CON1bits.ADDMABM = 1;
+	AD1CON1bits.ADDMABM = 1; 									//L: MMMMMMMMMMMMMMMMM......................
 
 	//Convierte CH0 (pero al tener el scan lee AN0 y AN1)
-	AD1CON2bits.CHPS = 0;
+	AD1CON2bits.CHPS = 0;										//L: ESTO TAMBIEN
 
 	//comienza a llenar buffer siempre desde el principio
-	AD1CON2bits.BUFM = 0;
+	AD1CON2bits.BUFM = 0;										//L: SI TMABIEN
 
 	// Escanea CH0+ en el los distintos ANx especificados
-	AD1CON2bits.CSCNA = 1;
+	AD1CON2bits.CSCNA = 1;										//L: ESTO SI
 
 	// Escanea AN0 Y AN1
 	AD1CSSH = 0x0000;
-	AD1CSSL = 0x0003; //AN0 y AN1
+	AD1CSSL = 0x0003; //AN0 y AN1								//L: MUCHO MUY BIEN
 
 	//incrementa el buffer de DMA luego de cada conversion (1 buffer)
-	AD1CON2bits.SMPI = 0;
+	AD1CON2bits.SMPI = 0;										//L: SI TRABAJAMOS CON UN BLOQUE DE MEMORIA DE 1 BUFFER ESTO QUEDA ASI
 
 	//Always uses channel input selects for Sample A
-	AD1CON2bits.ALTS = 0;
+	AD1CON2bits.ALTS = 0;										//L: SI TAMBIEN
 
 	//3 tads de sampleo 
-	AD1CON3bits.SAMC = 3;
-
+	AD1CON3bits.SAMC = 3;			
+																/*ESTOS DOS ME PARECE QUE HABRIA QUE DARLE 
+																UN POQUITO MAS DE TIEMPO DE SAMPLEO*/
 	//3 * TCY = TAD
-	AD1CON3bits.ADCS = 2;
+	AD1CON3bits.ADCS = 2;		
 
 	//DONE SE PONEN EN UNO CUANDO TERMINARON TODAS LAS CONVERSIONES DEL ADC (en nuestro caso cuando termina AN0 y AN1)
 
 	//necesitamos 1 buffer de 2 words
-	AD1CON4bits.DMABL = 0;
+	AD1CON4bits.DMABL = 0;										/*L: ESTO ME PARECE QUE TENDRIA QUE ESTAR EN 1 SI VAMOS A TRABAJAR
+																CON UN SOLO BLOQUE, DE 1 BUFFER Y 2 WORDS*/
 
 	// No queremos que el ADC interrumpa
 	// Clear the A/D interrupt flag bit
@@ -66,9 +72,10 @@ void Init_DMA( void )
     // IRQ: ADC Interrupt
 
     // Configure DMA for Peripheral indirect mode
-    DMA0CONbits.AMODE = 2;
+    DMA0CONbits.AMODE = 2;						
     // Configure DMA for Continuous Ping-Pong mode
-    DMA0CONbits.MODE = 0;
+    DMA0CONbits.MODE = 0;										//L: SI TRABAJMOS CON UN SOLO BLOQUE DE MEMORIA PARA ALMACENAR, ESTO QUEDARIA ASI.
+																//L: MODO CONTINUO, PING PONG DESHABILITADO
 
     // Point DMA to ADC1BUF0
     DMA0PAD = (unsigned int)&ADC1BUF0;
@@ -76,7 +83,7 @@ void Init_DMA( void )
     // 2 requerimientos de DMA (1 buffer ; 2 words)
     DMA0CNT = 1;
     // Select ADC1 as DMA Request source
-    DMA0REQ = 13;
+    DMA0REQ = 13;												/*ESTA TODO RE BIEN*/
 
     //localizo el buffer
     DMA0STA = __builtin_dmaoffset(&BufferA);
